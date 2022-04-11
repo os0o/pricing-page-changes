939efb621ebd72eb9104c7a97d1caa30ab8cc600
John Jordan <john.jordan@sendsafely.com>
2022-04-11 16:21:22 -0700

    refactor: remove positioning CSS, time delays with setTimeout, and UI timing aspect from custom-script.js

--- Pricing _ SendSafely _ Send Files Securely_files/custom-script.js
+++ Pricing _ SendSafely _ Send Files Securely_files/custom-script.js
@@ -1,41 +1,25 @@
-console.log('loading...');
-function showOrHideAllExceptLast(allElements, uiChangeTime, hideLast) {
-    var show = typeof hideLast !== 'undefined' ? hideLast : true;
-    var $firstElements = allElements.slice(0,-1);
-    if(show) {
-        $firstElements.removeClass('hideLeft opacityHide');
-        allElements.last().addClass('hideRight opacityHide');
-        setTimeout(function (){
-            $firstElements.show();
-            allElements.last().hide();
-        }, uiChangeTime);
-        return;
-    }
-    $firstElements.addClass('hideLeft opacityHide');
-    allElements.last().removeClass('hideRight opacityHide');
 
-    setTimeout(function (){
-        $firstElements.hide();
-        allElements.last().show();
-    }, uiChangeTime * 0.5);
+function showElemsHideOthers($elemsToShow, $elemsToHide) {
+    $elemsToShow.show().removeClass('opacityHide');
+    $elemsToHide.hide().addClass('opacityHide');
 }
+
 $(document).ready(function (){
     var $tabToggle = $('#pricing_plan_toggle');
     var $numUsersForm = $('#num_users_form');
-    var $priceTable = $('#home');
-    var $priceTableCols = $priceTable.find('.priceTable_col');
-    var uiTimeMs = 800;
-    console.log('ready');
-    $priceTableCols.slice(0,-1).hide();
-    $priceTableCols.last().removeClass('opacityHide');
+    var $rowEnterprise = $('#row_enterprise');
+    var $rowIndividual = $('#row_individual');
+
+    $rowIndividual.hide();
     $tabToggle.on('click', function (ev) {
         var $tab = $(ev.target);
-        if($tab.prop('id') === 'retail-tab') {
+
+        if($tab.prop('id') === 'individual-tab') {
             $numUsersForm.addClass('opacityHide');
-            showOrHideAllExceptLast($priceTableCols, uiTimeMs, true);
+            showElemsHideOthers($rowIndividual, $rowEnterprise);
             return;
         }
         $numUsersForm.removeClass('opacityHide');
-        showOrHideAllExceptLast($priceTableCols, uiTimeMs, false);
+        showElemsHideOthers($rowEnterprise, $rowIndividual);
     });
 });
